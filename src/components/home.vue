cd0048b237968ff27b2a978399b79951188850cf
qaqpop <49335855+qaqpop@users.noreply.github.com>
2023-01-12 16:49:41 +0800

    Create home.vue

---
+++ src/components/home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="home">
+  <h1>{{title1}}</h1>
+    <users v-bind:users="users"></users>
+  </div>
+</template>
+
+<script>
+import Users from './Users'
+
+export default {
+  name: 'home',
+  data(){
+   return{
+    title1:'This is a project',
+    users: [
+
+    ]
+   }
+  },
+  components: {
+    "users":Users,
+  },
+  created(){
+    this.$http.get("/api/weixin_42110432?type=lately")
+               .then((data)=>{
+                  this.users = data.body;
+        })
+  }
+}
+</script>
+
+<style>
+
+</style>
