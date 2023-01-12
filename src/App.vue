c8f9779af59fcd9f722b10f76d0c5a03147b2a09
qaqpop <49335855+qaqpop@users.noreply.github.com>
2023-01-12 16:51:22 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ul>
+      <li>
+        <router-link to="/helloworld">HelloWorld</router-link>
+        <router-link to="/">Home</router-link>
+        <router-link to="/element">Element</router-link>
+      </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data(){
+   return{
+
+   }
+ }
+}
+</script>
+
+<style>
+
+</style>
